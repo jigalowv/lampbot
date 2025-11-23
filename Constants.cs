8e8565de30645e40c073fab55cb1db8d4ea76922
jigalowv <vjigalowwork@gmail.com>
2025-11-23 17:25:59 +0200

    add: constants

---
+++ Constants.cs
@@ -0,0 +1,19 @@
+namespace lampbot
+{
+    public static class Constants
+    {
+        public const string DateTimeFormat = "dd.MM.yy HH:mm";
+        public const string TimeFormat = "HH:mm";
+        public const string DateFormat = "dd.MM.yy";
+        public const string Responses = "Responses";
+        public const string NotFound = "NotFound";
+        public const string NoAccess = "NoAccess";
+        public const string AlreadyExists = "AlreadyExists";
+        public const string Remove = "Remove";
+        public const string Add = "Add";
+        public const string Update = "Update";
+        public const string Invalid = "Invalid";
+        public const string Format = "Format";
+        public const string Access = "Access";
+    }
+}
